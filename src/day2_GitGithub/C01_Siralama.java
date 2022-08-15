67926257d183dc82b65587c34b69243f3282bed9
karanfil822 <karanfilkaranfil822@gmail.com>
2022-08-15 21:02:57 +0300

    v2

---
+++ src/day2_GitGithub/C01_Siralama.java
@@ -0,0 +1,19 @@
+package day2_GitGithub;
+
+public class C01_Siralama {
+    /*
+    1-git init ==> Local repo olusturmak icin yani .git ile klasorumuzun icindeki dosyalari
+    iliskilendirmek icin kullanilir
+    2-git add . ==>Working space'den (yerel) dosyalarimi statiging are'ya (committlemek icin beklenen yer)
+    gonderir
+    3-git status ==>
+    4-git commit -m"mesaj"==>Staging area'dan commit stora'a dosyalarimi gondermek icin kullanilir
+    (commit yani versiyon yani surum olusturmus olurum)
+    4-git push ==>Uzak repo'ya (yani remote -GitHub)gondermek icin kullandigimiz kod
+    Yalniz git push methodunu direkt kullanmak istersek 1 kez
+              git remote add orijin adress
+              git push -u orijin master
+              Not;Yukaridaki iki komutu tek seferde kullandiktan sonra ikinci commit'lerim icin sadece git
+              push kullaniriz
+     */
+}
