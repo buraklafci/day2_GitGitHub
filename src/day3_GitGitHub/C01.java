744444e86c7bcdc3e40747f6de5973db0d63dd46
karanfil822 <102259807+karanfil822@users.noreply.github.com>
2022-08-16 22:08:35 +0300

    Update C01.java

--- src/day3_GitGitHub/C01.java
+++ src/day3_GitGitHub/C01.java
@@ -4,5 +4,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("ogrendim galiba bu isi");
         System.out.println("ogrendim galiba bu isi ...");
+        System.out.println("ogrendim galiba bu isi ......");
     }
 }
