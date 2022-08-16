d3b059b06a0963bde58d568379ae7586dc3ce3d3
BURAK LAFCI <karanfilkaranfil822@gmail.com>
2022-08-16 22:54:33 +0300

    branch

--- src/day3_GitGitHub/Gitignore.java
+++ src/day3_GitGitHub/Gitignore.java
@@ -3,5 +3,6 @@ package day3_GitGitHub;
 public class Gitignore {
     public static void main(String[] args) {
         System.out.println("Gitignore Deneme 1");
+        System.out.println("Gitignore Deneme Branch");
     }
 }
