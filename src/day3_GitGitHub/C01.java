ddc2edf6f9b3510a38c599a4ea889d1e1b8b6c93
BURAK LAFCI <karanfilkaranfil822@gmail.com>
2022-08-16 22:05:03 +0300

    clone

--- src/day3_GitGitHub/C01.java
+++ src/day3_GitGitHub/C01.java
@@ -3,5 +3,6 @@ package day3_GitGitHub;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("ogrendim galiba bu isi");
+        System.out.println("ogrendim galiba bu isi ...");
     }
 }
